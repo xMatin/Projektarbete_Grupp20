63f3ede01177f85ee0438e338ecee14bbdff124e
xMatin <h22mehta@du.se>
2022-12-28 11:02:33 +0100

    Initial commit

---
+++ Projektarbete_Grupp20/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projektarbete_Grupp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
